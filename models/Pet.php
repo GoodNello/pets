fadcab414aa111a62c9bd5d43284c20e6b40de89
GoodNello <grislysquirrel@gmail.com>
2016-10-13 15:03:47 +0200

    Removed old relations

--- models/Pet.php
+++ models/Pet.php
@@ -12,12 +12,8 @@ class Pet extends Model
 
     public $timestamps = false;
 
-    public $belongsTo = [
-        'user' => ['RainLab\User\Models\User']
-    ];
-
-    protected $guarded = [
-        'user_id'
+    public $belongsToMany = [
+        'owners' => ['RainLab\User\Models\User', 'table' => 'goodnello_pets_owners']
     ];
 
     protected $fillable = [
@@ -40,29 +36,12 @@ class Pet extends Model
             $user->pet = $pet;
             $pet->setRelation('user', $user);
         }
-        else {
-            $pet = new static;
-            $pet->user = $user;
-            $pet->save();
-<<<<<<< HEAD
-
-            $user->pet = $pet;
-
-            return $pet;
-        }
 
     }
 
     public function getUserOptions($keyValue = null) {
 
         $users_name = Db::table('users')->lists('username');
-=======
-
-            $user->pet = $pet;
-
-            return $pet;
-        }
->>>>>>> cdd1f6b61c588863284df7c1de60409f0bdfd7e8
 
         return $users_name;
     }
