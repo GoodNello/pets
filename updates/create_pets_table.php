53d290eb1f970e1b894d406a1739969aa1c01b77
GoodNello <grislysquirrel@gmail.com>
2016-10-15 13:05:25 +0200

    Added check for table existance

--- updates/create_pets_table.php
+++ updates/create_pets_table.php
@@ -8,16 +8,18 @@ class CreatePetsTable extends Migration
 {
     public function up()
     {
-        Schema::create('goodnello_pets_pets', function(Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->integer('owner_id')->unsigned()->index();
-            $table->text('name')->nullable();
-            $table->char('genus', 10)->nullable();
-            $table->string('species')->nullable();
-            $table->date('birth')->nullable();
-            $table->text('description')->nullable();
-        });
+        if (!Schema::hasTable('goodnello_pets_pets')) {
+            Schema::create('goodnello_pets_pets', function(Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->increments('id');
+                $table->integer('owner_id')->unsigned()->index();
+                $table->text('name')->nullable();
+                $table->char('genus', 10)->nullable();
+                $table->string('species')->nullable();
+                $table->date('birth')->nullable();
+                $table->text('description')->nullable();
+            });
+        }
     }
 
     public function down()
