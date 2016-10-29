726526e3a799aee230c72c1ab3b11485d60404ec
GoodNello <grislysquirrel@gmail.com>
2016-10-29 16:11:08 +0200

    Added birth accessor

--- models/Pet.php
+++ models/Pet.php
@@ -2,6 +2,7 @@
 
 use Model;
 use Db;
+use Carbon\Carbon as Carbon;
 use October\Rain\Database\Traits\Validation as Validation;
 use GoodNello\Pets\Models\Pet as PetModel;
 
@@ -53,28 +54,13 @@ class Pet extends Model
     }
 
     protected function listSpecies() {
-        return json_decode(file_get_contents(__DIR__.'/../data/species.json'), true);
+        return json_decode(file_get_contents(__DIR__.'/../data/species.json'));
     }
 
     // Lists species for creation/editing
     public function getSpeciesOptions($keyValue = null) {
         //I don't know why, but this keeps resetting when editing the model
         $species = array_map('ucfirst', $this->listSpecies());
-
-        if($this->species) {
-            $key = array_search($this->species, $species);
-            array_unshift($species, $this->species);
-
-            /*
-
-            $colours = array("red", "green", "blue", "yellow");
-
-            $movecolour = $colours[2];
-            unset($colours[2]);
-            array_unshift($colours, $movecolour);
-            */
-
-        }
 
         return $species;
     }
@@ -86,6 +72,13 @@ class Pet extends Model
         $value = $species[($value ?: 0)];
 
         return $value;
+    }
+
+    protected function getBirthAttribute($value) {
+
+        $formatted = Carbon::createFromFormat('Y-m-d', $value)->formatLocalized('%d/%m/%Y'); ;
+
+        return $formatted;
     }
 
 }
